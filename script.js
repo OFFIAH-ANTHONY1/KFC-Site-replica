2ba764987330177fd5a23923c3e1c931e49a75dc
OFFIAH ANTHONY1 <ebubeoffiah95@gmail.com>
2024-07-31 23:41:46 +0100

    updated

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+//outside block variables
 const menu = document.getElementById("menu");
+const searchIcon = document.getElementById("search-icon");
+const backIcon = document.getElementById("back-icon");
 
+//display menu bar
 function displayMenuBar(){
     const menuBar = document.getElementById("menubar");
     menuBar.style.display="block";
 }
 
-
 menu.addEventListener("click", displayMenuBar);
+
+//hide menu bar
+function hideMenuBar(){
+    const menuBar = document.getElementById("menubar");
+    menuBar.style.display="none";
+}
+
+backIcon.addEventListener("click", hideMenuBar);
+
+//display search input
+
+function displaySearchInput(){
+    const search = document.getElementById("search");
+    search.style.display="block";
+}
+
+searchIcon.addEventListener("click", displaySearchInput);
